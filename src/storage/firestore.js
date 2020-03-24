d9e7cedd791640af7b64f79ed15994b51d974630
Thomas <thomas@Thomass-MBP.fritz.box>
2020-03-24 10:35:43 +0100

    fixed error

--- src/storage/firestore.js
+++ src/storage/firestore.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin')
 const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert(config.get('CLOUD.FIRESTORE'))
 })
-firebaseApp.auth().
+
 module.exports = {
   firestore: firebaseApp.firestore(),
   admin: firebaseApp.auth()
